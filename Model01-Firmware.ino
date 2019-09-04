82aed126e3949ba3e97acca0e7c4cf168280aeac
nickzylstra <nick.zylstra@gmail.com>
2019-09-03 17:24:59 -0700

    clean up qwerty layer for modifiers

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -306,19 +306,19 @@ KEYMAPS(
    ___),
 
   [QWERTY] = KEYMAP_STACKED
-  (___,             Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick,    Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,      Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown,    Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Spacebar, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNC),
+  (___, Key_1, Key_2, Key_3, Key_4, Key_5, ___,
+   ___, Key_Q, Key_W, Key_E, Key_R, Key_T, ___,
+   ___, Key_A, Key_S, Key_D, Key_F, Key_G,
+   ___, Key_Z, Key_X, Key_C, Key_V, Key_B, ___,
+   ___, ___, ___, ___,
+   ___,
 
-   ___,           Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_LeftGui,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_RightAlt,  Key_Backspace, Key_RightControl,
-   ShiftToLayer(FUNC))
+   ___,   Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
+   ___,   Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+          Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   ___,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   ___, ___, ___, ___,
+   ___)
 ) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
